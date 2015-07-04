a458ac004a8763ae8e12a2acf293d7e837bb9796
Abner Castro <abner.abbey@hotmail.com>
2015-07-03 22:54:24 -0500

    Initial Commit

---
+++ Monk Admin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Monk Admin
+//
+//  Created by Abner Castro Aguilar on 03/07/15.
+//  Copyright (c) 2015 Abner Castro Aguilar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Monk AdminTests/Monk_AdminTests.m
@@ -0,0 +1,40 @@
+//
+//  Monk_AdminTests.m
+//  Monk AdminTests
+//
+//  Created by Abner Castro Aguilar on 03/07/15.
+//  Copyright (c) 2015 Abner Castro Aguilar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Monk_AdminTests : XCTestCase
+
+@end
+
+@implementation Monk_AdminTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
